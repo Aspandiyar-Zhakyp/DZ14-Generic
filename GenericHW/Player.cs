6d8c1007ae4c845378fb15f6295c57dec138467b
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-16 20:53:42 +0600

    DZ14 / HW

---
+++ GenericHW/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericHW
+{
+    public class Player
+    {
+        public List<Card> cards = new List<Card>();
+    }
+
+    public enum CardValue
+    {
+        SIX = 0, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    public enum CardSuit
+    {
+        HEARTS = 0, DIAMONDS, CLUBS, SPADES
+    }
+    public class Card
+    {
+
+        public readonly CardValue value;
+        public readonly CardSuit suit;
+
+        public Card(CardValue value, CardSuit suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
+
+        public override string ToString()
+        {
+            return $"{value} {suit}";
+        }
+    }
+}
